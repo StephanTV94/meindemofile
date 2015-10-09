db6a560034399123ecb452e10e2f7355111a7868
StephanTV1994 <stephan.terveer@gmail.com>
2015-10-09 20:22:18 +0200

    lolololol

--- app/src/main/java/com/example/stephan/meinscheissynchro/MainActivity.java
+++ app/src/main/java/com/example/stephan/meinscheissynchro/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            System.out.println("Hallo Weelt");
+            System.out.println("Hallo Wedelt");
             return true;
         }
 
